80f445c370489ba2b2e47f4d32ffbb954b02275e
Gleb <gapodgorsky@yandex.ru>
2022-10-04 15:39:54 +0300

    Добавление пояснений в код программы

--- My_Final_Programm_In_Quarter/Program.cs
+++ My_Final_Programm_In_Quarter/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 Console.Clear();
-string[] startStr = new string[] {"hello", "2", "world", ":-)"};
-string[] endStr = new string[startStr.Length];
-void PutDigitIfLessThree(string[] startStr, string[] endStr)
+string[] startStr = new string[] {"hello", "2", "world", ":-)"}; // объявляем изначальную строку с заданными значениями
+string[] endStr = new string[startStr.Length]; // объявляем вторую, пустую строку соответствующую размерам первой
+void PutDigitIfLessThree(string[] startStr, string[] endStr) // Прописываем метод поиска в первой строке значений < = 3
 {
-    int count = 0;
+    int count = 0; 
     for (int i = 0; i < startStr.Length; i++)
     {
         if (startStr[i].Length <= 3)
@@ -14,7 +14,7 @@ void PutDigitIfLessThree(string[] startStr, string[] endStr)
         }
     }
 }
-void PrintString(string[] array)
+void PrintString(string[] array) // Метод распечатки строки
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -22,6 +22,6 @@ void PrintString(string[] array)
     }
     Console.WriteLine();
 }
-PutDigitIfLessThree(startStr, endStr);
+PutDigitIfLessThree(startStr, endStr); // Вызов написанных методов
 Console.Write("Строка со значениями которые меньше трех:            ");
 PrintString(endStr);
