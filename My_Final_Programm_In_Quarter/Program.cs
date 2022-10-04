0ec9259d7e09a0112e58a222c04a804fdf7c7fa0
Gleb <gapodgorsky@yandex.ru>
2022-10-04 15:19:57 +0300

    Наведение марафета в программе

--- My_Final_Programm_In_Quarter/Program.cs
+++ My_Final_Programm_In_Quarter/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 Console.Clear();
-string[] startS = new string[] {"hello", "2", "world", ":-"};
-string[] endS = new string[startS.Length];
-void PutDigitIfLessThree(string[] startS, string[] array2)
+string[] startStr = new string[] {"hello", "2", "world", ":-)"};
+string[] endStr = new string[startStr.Length];
+void PutDigitIfLessThree(string[] startStr, string[] endStr)
 {
     int count = 0;
-    for (int i = 0; i < startS.Length; i++)
+    for (int i = 0; i < startStr.Length; i++)
     {
-        if (startS[i].Length <= 3)
+        if (startStr[i].Length <= 3)
         {
-            endS[count] = startS[i];
+            endStr[count] = startStr[i];
             count++;
         }
     }
 }
-void PrintArray(string[] array)
+void PrintString(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -22,5 +22,6 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
-PutDigitIfLessThree(startS, endS);
-PrintArray(endS);
+PutDigitIfLessThree(startStr, endStr);
+Console.Write("Строка со значениями которые меньше трех:            ");
+PrintString(endStr);
